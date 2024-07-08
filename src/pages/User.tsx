453223eef901abd413c0ab8d3a799e911545c68b
PdcShoun <pdc.pachara@gmail.com>
2024-07-08 18:25:10 +0700

    user next prev button

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,19 +1,25 @@
 import { getUserDetail } from '@/resource/user'
-import { useParams } from '@solidjs/router'
+import { useParams, A } from '@solidjs/router'
 import { Show, createResource } from 'solid-js'
-
 
 const User = () => {
   const params = useParams()
   const userId = () => params.id
+  const nextId = () => Number(userId()) + 1
+  const prevId = () => Number(userId()) - 1
   const [userData] = createResource(userId, getUserDetail)
   return (
     <div class="">
-      User {userId()}
+      User id: {userId()}
       <div>
-        <Show when={true}>data:</Show>
-        {JSON.stringify(userData()?.firstName)}
+        <Show when={!userData.loading} fallback={<>Loding</>}>
+          first name: {userData()?.firstName}
+        </Show>
       </div>
+      <Show when={prevId() > 0}>
+        <A href={`/users/${prevId()}`}>Prev</A>
+      </Show>
+      <A href={`/users/${nextId()}`}>Next</A>
     </div>
   )
 }
